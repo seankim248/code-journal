bc9f35770b19235d25b73c4e6bb94016ed7189a2
Sean Kim <seankim2480@gmail.com>
2021-12-21 12:49:55 -0800

    Used a loop to create a DOM tree for each journal entry object in the data model and appended it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ var $photo = document.querySelector('.photo');
 var $form = document.querySelector('form');
 var $title = document.querySelector('.title');
 var $notes = document.querySelector('.notes');
-var $entry = document.querySelector('a');
-var $entryInfo = document.querySelector('.entryInfo');
+var $header = document.querySelector('header');
+var $ul = document.querySelector('ul');
+var $views = document.querySelectorAll('.view');
 
 $photoUrl.addEventListener('input', function (e) {
   $photo.setAttribute('src', e.target.value);
@@ -28,7 +29,50 @@ $form.addEventListener('submit', function (e) {
   $form.reset();
 });
 
-$entry.addEventListener('click', function (e) {
-  $form.classList.add('hidden');
-  $entryInfo.classList.remove('hidden');
+$header.addEventListener('click', function (e) {
+  if (e.target.matches('.tab')) {
+    var dataView = e.target.getAttribute('data-view');
+    for (var i = 0; i < $views.length; i++) {
+      if ($views[i].getAttribute('data-view') === dataView) {
+        $views[i].className = 'view';
+      } else {
+        $views[i].className = 'view hidden';
+      }
+    }
+  }
 });
+
+function createEntryDOMTree(obj) {
+  var $li = document.createElement('li');
+
+  var $row = document.createElement('div');
+  $row.className = 'row margin-bottom';
+  $li.appendChild($row);
+
+  var $columnHalf = document.createElement('div');
+  $columnHalf.className = 'column-half';
+  $row.appendChild($columnHalf);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $img.setAttribute('alt', 'placeholder');
+  $columnHalf.appendChild($img);
+
+  var $secondColumnHalf = document.createElement('div');
+  $secondColumnHalf.className = 'column-half';
+  $row.appendChild($secondColumnHalf);
+
+  var $h3 = document.createElement('h3');
+  $h3.textContent = 'Placeholder Image';
+  $secondColumnHalf.appendChild($h3);
+
+  var $p = document.createElement('p');
+  $p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore accusantium quaerat iure. Repudiandae magni, libero numquam reiciendis atque voluptates id fugit distinctio facilis asperiores at. Facilis corrupti, dolor sed assumenda laboriosam rerum iste fugit dolores vero nam earum placeat amet doloremque, magnam eaque unde, sint impedit iure officia minima magni!';
+  $secondColumnHalf.appendChild($p);
+
+  return $li;
+}
+
+for (var i = 0; i < data.entries.length; i++) {
+  $ul.appendChild(createEntryDOMTree(data.entries[i]));
+}
