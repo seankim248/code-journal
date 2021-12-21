2df5698713830a0e97dc04a9dcc7a70663afe222
Sean Kim <seankim2480@gmail.com>
2021-12-20 16:33:27 -0800

    Listened for events on the journal entry form and others

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@
 
 var $photoUrl = document.querySelector('.photo-url');
 var $photo = document.querySelector('.photo');
+var $form = document.querySelector('form');
+var $inputs = document.querySelector('form').elements;
 
 $photoUrl.addEventListener('input', function (e) {
   $photo.setAttribute('src', e.target.value);
+});
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var obj = {};
+  for (var i = 0; i < $inputs.length; i++) {
+    obj[$inputs[i].name] = $inputs[i].value;
+  }
+  obj.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(obj);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
